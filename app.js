b791b73624e608da8a457128a6d92480614789c8
ChaseDev <chasewebdev101@gmail.com>
2023-03-21 23:35:47 -0700

    Update style and HTML

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-const display = document.querySelector(".display");
-const emptyValue = document.createElement("div");
-emptyValue.classList.add(".empty-value");
-emptyValue.textContent = "";
-const btnOne = document.querySelector("btn-one");
-
-btnOne.addEventListener("click", runOne);
-
-function runOne () {
-    emptyValue.textContent = "1";
-    display.appendChild(emptyValue);
-}
+const numberButons = document.querySelectorAll("[data-number");
