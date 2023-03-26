ef1616ca3c427bbdd0c7ba7107e9133d8768a94c
ChaseDev <chasewebdev101@gmail.com>
2023-03-26 14:49:28 -0700

    Fix decimal button

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-let numValue = [];
+let firstValue = [];
+let secondValue = [];
 
-const currentCalc = document.querySelector(".current-calc");
+let currentCalc = document.querySelector(".current-calc");
 const btn = document.querySelectorAll("button");
 const btnNumber = document.querySelectorAll(".btn-number");
 const btnClear = document.querySelector(".btn-clear");
 const btnDecimal = document.querySelector(".btn-decimal");
+const btnOperand = document.querySelectorAll(".btn-operand");
+let operandValue = "empty";
+const btnEquals = document.querySelector(".btn-equals");
 
 btnNumber.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -13,10 +17,34 @@ btnNumber.forEach(btn => {
 });
 
 function addCode(num) {
-    numValue.push(num);
-    const numString = numValue.join("");
-    currentCalc.textContent = "";
-    currentCalc.append(numString);
+    if (operandValue != "empty") {
+        currentCalc.textContent = secondValue;
+        secondValue.push(num);
+        const numString = firstValue.join("") + " " + operandValue + " " + secondValue.join("");
+        currentCalc.textContent = numString;
+    } else {
+        currentCalc.textContent = firstValue;
+        firstValue.push(num);
+        const numString = firstValue.join("");
+        currentCalc.textContent = numString;
+    };
+}
+
+btnOperand.forEach(btn => {
+    btn.addEventListener("click", () => {
+        addOperand(btn.textContent);
+    });
+});
+
+function addOperand(operand) {
+    if (operandValue != "empty") {
+        btnOperand.removeEventListener;
+    } else {
+        operandValue = operand;
+        const numString = firstValue.join("") + " " + operandValue + " " + secondValue.join("");
+        currentCalc.textContent = numString;
+        console.log(operandValue);
+    };
 }
 
 btnDecimal.addEventListener("click", () => {
@@ -24,21 +52,60 @@ btnDecimal.addEventListener("click", () => {
 });
 
 function addDecimal (decimal) {
-    if (numValue.includes(".")) {
-        btnDecimal.removeEventListener;
+    if (operandValue === "empty") {
+        if (firstValue.includes(".") || firstValue.includes("0.")) {
+            btnDecimal.removeEventListener;
+        } else {
+            if (firstValue.length === 0) {
+                firstValue.push("0" + decimal);
+                const decimalString = firstValue.join("");
+                currentCalc.textContent = "";
+                currentCalc.append(decimalString);
+            } else {
+                firstValue.push(decimal);
+                const decimalString = firstValue.join("");
+                currentCalc.textContent = "";
+                currentCalc.append(decimalString);
+            };
+        };
     } else {
-        numValue.push(decimal);
-        const decimalString = numValue.join("");
-        currentCalc.textContent = "";
-        currentCalc.append(decimalString);
-        console.log(numValue);
+        if (secondValue.includes(".") || secondValue.includes("0.")) {
+            btnDecimal.removeEventListener;
+        } else {
+            if (secondValue.length === 0) {
+                secondValue.push("0" + decimal);
+                const decimalString = firstValue.join("") + " " + operandValue + " " + secondValue.join("");
+                currentCalc.textContent = "";
+                currentCalc.append(decimalString);
+            } else {
+                secondValue.push(decimal);
+                const decimalString = firstValue.join("") + " " + operandValue + " " + secondValue.join("");
+                currentCalc.textContent = "";
+                currentCalc.append(decimalString);
+            };
+        };
     };
 }
+
+// btnEquals.addEventListener("click", runEquals);
+
+// function runEquals() {
+//     const firstString = firstValue.toString();
+//     const secondString = secondValue.toString();
+//     const operandString = "empty";
+//     if (operandValue == "+") {
+//         const addString = firstString + secondString
+//     } 
+//     const totalValue = 
+//     currentCalc.textContent = totalValue;
+//     console.log(totalValue);
+// }
 
 btnClear.addEventListener("click", runClear);
 
 function runClear() {
-    numValue = [];
+    firstValue = [];
+    secondValue = [];
+    operandValue = "empty"
     currentCalc.textContent = "";
-    console.log(numValue);
 }
