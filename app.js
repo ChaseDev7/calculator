5cec7334bcb2b366370c5e15a0c179a90f617066
ChaseDev <chasewebdev101@gmail.com>
2023-03-25 22:23:24 -0700

    Add functions for nums and clear

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-const numberButons = document.querySelectorAll("[data-number");
+let numValue = [];
+
+const currentCalc = document.querySelector(".current-calc");
+const btn = document.querySelectorAll("button");
+const btnNumber = document.querySelectorAll(".btn-number");
+const btnClear = document.querySelector(".btn-clear");
+const btnDecimal = document.querySelector(".btn-decimal");
+
+btnNumber.forEach(btn => {
+    btn.addEventListener("click", () => {
+        addCode(btn.textContent);
+    });
+});
+
+function addCode(num) {
+    numValue.push(num);
+    const numString = numValue.join("");
+    currentCalc.textContent = "";
+    currentCalc.append(numString);
+}
+
+btnDecimal.addEventListener("click", () => {
+    addDecimal(".");
+});
+
+function addDecimal (decimal) {
+    if (numValue.includes(".")) {
+        btnDecimal.removeEventListener;
+    } else {
+        numValue.push(decimal);
+        const decimalString = numValue.join("");
+        currentCalc.textContent = "";
+        currentCalc.append(decimalString);
+        console.log(numValue);
+    };
+}
+
+btnClear.addEventListener("click", runClear);
+
+function runClear() {
+    numValue = [];
+    currentCalc.textContent = "";
+    console.log(numValue);
+}
